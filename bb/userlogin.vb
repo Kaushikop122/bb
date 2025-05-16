fb153ceb909afa63ad3ec145960da5af34ec6b2b
Kaushikop122 <kaushikkop12@gmail.com>
2025-05-16 16:41:45 +0530

    safe commit n

--- bb/userlogin.vb
+++ bb/userlogin.vb
@@ -1,14 +1,21 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class userlogin
+    Dim connectionString As String = "Data Source=NITRO5\MSSQLSERVER01;Initial Catalog=BudgetBud;Integrated Security=True;TrustServerCertificate=True"
+
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
-        Dim conn As New SqlConnection("Data Source=NITRO5\MSSQLSERVER01;Initial Catalog=BudgetBud;Integrated Security=True")
+        If txtEmail.Text = "" OrElse txtPassword.Text = "" Then
+            MessageBox.Show("Please enter Email and Password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+        Dim conn As New SqlConnection(connectionString)
         Dim cmd As New SqlCommand("SELECT userid FROM Usertable WHERE Email = @Email AND Password = @Password", conn)
 
         cmd.Parameters.AddWithValue("@Email", txtEmail.Text)
         cmd.Parameters.AddWithValue("@Password", txtPassword.Text)
 
         conn.Open()
+
         Dim reader As SqlDataReader = cmd.ExecuteReader()
 
         If reader.Read() Then
@@ -27,6 +34,8 @@ Public Class userlogin
                 ' ✅ Open Dashboard only once
                 Dim dashboard As New userdashboard()
                 dashboard.Show()
+                'userdashboard.Show()
+
                 Me.Hide()
                 LoggedInUserEmail = txtEmail.Text
                 LoggedInUserPassword = txtPassword.Text
@@ -34,10 +43,6 @@ Public Class userlogin
                 MessageBox.Show("Invalid email or password!", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
         End If
-
-
-
-
         conn.Close()
     End Sub
 
